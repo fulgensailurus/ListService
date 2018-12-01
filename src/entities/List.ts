c6069aad55e0fd16e606150d137b7bec9398317f
Roeland <roeland.matthijssens@vikingco.com>
2018-12-02 00:08:57 +0100

    Eagerly load purchase relation on lists
    We need at least the IDs of the lists' purchases so a join is required
    anyway. Fetching the additional data doesn't cost us anything so we
    embed the purchase object.

--- src/entities/List.ts
+++ src/entities/List.ts
@@ -1,7 +1,13 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Purchase } from './Purchase'
 
 @Entity('lists')
 export class List extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @OneToMany(type => Purchase, purchase => purchase.list, {
+    eager: true
+  })
+  purchases: Purchase[];
 }
