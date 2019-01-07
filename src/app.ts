459ed3656d666337155a94d3740dd01f0ee60692
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2019-01-07 20:36:18 +0100

    Add routes to resources

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import 'reflect-metadata';
 import fastify from 'fastify';
 import items from './routes/items';
+import lists from './routes/lists';
+import purchases from './routes/purchases';
 
 const app = fastify({
   logger: process.env.NODE_ENV !== 'test' || Boolean(process.env.FORCE_LOGGING),
 });
 
 app.register(items, { prefix: '/items' });
+app.register(lists, { prefix: '/lists' });
+app.register(purchases, { prefix: '/purchases' });
 
 export = app;
