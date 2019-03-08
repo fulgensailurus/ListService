43392ad9d59e936b44a763ad9d9e500c5fb981ef
Jeffrey Vandenborne <jeffrey@vandenborne.co>
2019-03-08 20:06:32 +0100

    Add owner field to list entity + create migration

--- src/entities/List.ts
+++ src/entities/List.ts
@@ -1,10 +1,13 @@
-import { BaseEntity, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { BaseEntity, Entity, PrimaryGeneratedColumn, OneToMany, Column } from 'typeorm';
 import { Purchase } from './Purchase';
 
 @Entity('lists')
 export class List extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
+  owner: string;
 
   @OneToMany(type => Purchase, purchase => purchase.list, {
     eager: true,
